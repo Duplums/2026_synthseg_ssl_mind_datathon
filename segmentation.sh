4d9a82943efe194ed577a71000bad2ef7c567dd2
Benoit Dufumier <ben.dufumier@gmail.com>
2026-04-08 08:54:24 +0200

    first seg map

---
+++ segmentation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pixi run python ./legacy/SynthSeg/scripts/commands/SynthSeg_predict.py \
+    --i ./data/sub-109861838866_T1w.nii.gz \
+    --o ./segmentations/ \
+    --cpu --threads 55 --parc --robust \
+    --vol \
+    ./segmentations/volumes.csv \
+    --qc ./segmentations/qc.csv
